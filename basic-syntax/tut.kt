e57ac0aa181cd4626201cfc421e018a3d8e6ef9b
SamatarMumin <35471795+SamatarMumin@users.noreply.github.com>
2018-12-11 17:18:49 -0500

    Adding more syntax

--- basic-syntax/tut.kt
+++ basic-syntax/tut.kt
@@ -1,12 +1,13 @@
 package tut
 
+import java.util.*
+
 //Declaring main function
 fun main(args : Array<String>){
 
- //Declaring variables
+//Declaring variables
     var myage = 43 //Mutable
     var ChangeName = "Sam"
-
     val NoChange = "Ummutable" //Immutable
 
 //If Statement
@@ -15,17 +16,16 @@ fun main(args : Array<String>){
     } else
         println("okay cool")
 
-    //Built-Functions
+//Built-Functions
     var bigINT: Int = Int.MAX_VALUE
     var smallINT: Int = Int.MIN_VALUE
     println("Biggest Int: "  + bigINT)
     println("Smallest INT: $smallINT" ) //String imporlation
 
-    //Precision of numbers only of 15 Digits for Floats
 
-
+//String imporalation
     var letterGrade: Char = 'A'
-    println("A is a char : ${letterGrade is Char}" ) //String imporalation
+    println("A is a char : ${letterGrade is Char}" )
 
     //Casting variables
     println("3.14 to INT :" + (3.14.toInt()))
@@ -44,6 +44,22 @@ for(x in 1..10){
     println(x)
 }
 
+//While Loop
+    var guessnum = 0
+    while(guessnum < 10){
+    println(guessnum)
+   guessnum = guessnum + 2
+    }
+
+
+//List
+    val item = listOf(1,2,3,6,7,31,41,62,3)
+    val item2 = listOf(11,21,341,62,7,31,41,62,3)
+    val union = item.union(item2)
+    println(union)
+
+
+
 
     //Arrays
     var MyArray = arrayOf(2, 4.23 , "Hello")
@@ -52,6 +68,8 @@ for(x in 1..10){
     println(MyArray.size)
     println(MyArray.contains("1"))
 
-
+    
 
 }
+//Classes:
+open class Person(val type: String, var weight: Double){}
