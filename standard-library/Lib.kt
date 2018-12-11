25faa7302efaa35955548725179d462330f7bc56
SamatarMumin <35471795+SamatarMumin@users.noreply.github.com>
2018-12-11 17:30:04 -0500

    Adding more Lib

--- standard-library/Lib.kt
+++ standard-library/Lib.kt
@@ -3,11 +3,8 @@ package tut
 
 //Declaring main function
 fun main(args : Array<String>) {
-    var ChangeName = "Sam"
-    if (ChangeName == "Sam" ){
-       ChangeName.capitalize()
-    } else
-    ChangeName.toLowerCase()
+    val original = "SamatarMumin"
+
 
     val item = listOf(1,2,3,6,7,31,41,62,3)
     val item2 = listOf(11,21,341,62,7,31,41,62,3)
@@ -15,21 +12,30 @@ fun main(args : Array<String>) {
     val intersect = item.intersect(item)
     val reverse = item.asReversed()
 
+
+    original.let {
+        println("The String is $it")
+        it.reversed()
+    }.let {
+        println("The reverse is $it")
+        it.length
+    }.let {
+        println("The length is $it")
+    }
+
     println(union)
     println(intersect)
     println(reverse)
 
-   item.groupBy {
-       if (it % 2 == 0)
-       {
-        println("Even")
+    item.groupBy {
+        if (it % 2 == 0)
+        {
+            println("Even")
 
-       } else
-       {
-           println("Odd")
-       }
+        } else
+        {
+            println("Odd")
+        }
 
-
-
-   }
     }
+}
